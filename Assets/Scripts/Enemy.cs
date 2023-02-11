abd72d3a5948c78c7abfd3724cab8beb040fbeec
Morgan Houston <mmhouston6@live.com>
2023-02-11 01:08:10 -0600

    Spider WIP
    Baked NavMesh, Baked Occlusion Culling. Grass & Dirt Block Tri's need to be reduced

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Com.MorganHouston.Imprecision
+{
+    public class Enemy : MonoBehaviour
+    {
+
+        public Transform target;
+        private NavMeshAgent agent;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+}
