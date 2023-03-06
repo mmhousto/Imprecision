2f2b10e9ef0f8e5c72b5536c3ac54985417e9a74
Morgan Houston <mmhouston6@live.com>
2023-03-06 13:10:57 -0600

    Optimized Dirt Block. WIP Level 1

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,18 +9,54 @@ namespace Com.MorganHouston.Imprecision
     {
 
         public Transform target;
+        private Vector3 spawnLocation;
         private NavMeshAgent agent;
+        private bool following;
 
         // Start is called before the first frame update
         void Start()
         {
             agent = GetComponent<NavMeshAgent>();
+            spawnLocation = transform.position;
         }
 
         // Update is called once per frame
         void Update()
         {
-            agent.SetDestination(target.position);
+            Follow();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                following = true;
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                following = false;
+            }
+        }
+
+        private void Follow()
+        {
+            if (following)
+            {
+                FollowTarget(transform.position);
+            }
+            else
+            {
+                FollowTarget(spawnLocation);
+            }
+        }
+
+        private void FollowTarget(Vector3 targetPos)
+        {
+            agent.SetDestination(targetPos);
         }
     }
 }
