a8c2db4fe484ab26ebce8db93d070ce1c2fbf672
Morgan Houston <mmhouston6@live.com>
2024-03-21 13:15:41 -0500

    Arrow Impact Sounds (Tree/Wood, Ground, Water, Enemy)

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -11,7 +11,14 @@ namespace Com.MorganHouston.Imprecision
         Rigidbody arrowRB;
         public GameObject applePS;
         public AudioClip explosionTarget;
+        public AudioClip[] hitTree;
+        public AudioClip[] hitGround;
+        public AudioClip[] hitWater;
+        public AudioClip[] hitEnemy;
         public AudioMixerGroup sfx;
+
+        private GameObject audioSourceObject;
+        private AudioSource audioSource;
 
         private float lifeTimer = 6f;
         private float timer;
@@ -23,6 +30,21 @@ namespace Com.MorganHouston.Imprecision
             timer = lifeTimer;
             arrowRB = GetComponent<Rigidbody>();
             hit = false;
+
+            if (GameObject.FindWithTag("ArrowImpact") != null)
+            {
+                audioSourceObject = GameObject.FindWithTag("ArrowImpact");
+                audioSource = audioSourceObject.GetComponent<AudioSource>();
+            }
+            else
+            {
+                audioSourceObject = new GameObject("ArrowImpactSound");
+                audioSourceObject.tag = "ArrowImpact";
+                audioSource = audioSourceObject.AddComponent<AudioSource>();
+                audioSource.playOnAwake = false;
+                audioSource.spatialBlend = 1;
+            }
+
         }
 
         // Update is called once per frame
@@ -30,7 +52,6 @@ namespace Com.MorganHouston.Imprecision
         {
             DestroyArrowTTL();
             
-
         }
 
         private void DestroyArrowTTL()
@@ -58,22 +79,56 @@ namespace Com.MorganHouston.Imprecision
             hit = true;
             transform.SetParent(collision.gameObject.transform, true); // attach to target.
 
-            if (collision.collider.tag == "target")
-            {
-                
-                GameObject audioExplosion = new GameObject("AudioExplosion");
-                audioExplosion.transform.position = collision.transform.position;
-                AudioSource audio = audioExplosion.AddComponent<AudioSource>();
-                audio.clip = explosionTarget;
-                audio.playOnAwake = false;
-                audio.spatialBlend = 1;
-                audio.minDistance = 8;
-                audio.maxDistance = 75;
-                audio.outputAudioMixerGroup = sfx;
-                audio.Play();
-                Destroy(audioExplosion, 1f);
+            int rand;
 
+            switch (collision.collider.tag)
+            {
+                case "target":
+                    audioSource.clip = explosionTarget;
+                    audioSource.volume = 1;
+                    audioSource.minDistance = 8;
+                    audioSource.maxDistance = 75;
+                    break;
+                case "tree":
+                    rand = Random.Range(0, hitTree.Length);
+                    audioSource.clip = hitTree[rand];
+                    audioSource.volume = 0.3f;
+                    audioSource.minDistance = 0.5f;
+                    audioSource.maxDistance = 15;
+                    break;
+                case "ground":
+                    rand = Random.Range(0, hitGround.Length);
+                    audioSource.clip = hitGround[rand];
+                    audioSource.volume = 0.3f;
+                    audioSource.minDistance = 0.1f;
+                    audioSource.maxDistance = 10;
+                    break;
+                case "water":
+                    rand = Random.Range(0, hitWater.Length);
+                    audioSource.clip = hitWater[rand];
+                    audioSource.volume = 0.3f;
+                    audioSource.minDistance = 0.5f;
+                    audioSource.maxDistance = 25;
+                    break;
+                case "Enemy":
+                    rand = Random.Range(0, hitEnemy.Length);
+                    audioSource.clip = hitEnemy[rand];
+                    audioSource.volume = 0.3f;
+                    audioSource.minDistance = 0.5f;
+                    audioSource.maxDistance = 15;
+                    break;
+                default:
+                    rand = Random.Range(0, hitGround.Length);
+                    audioSource.clip = hitGround[rand];
+                    audioSource.volume = 0.3f;
+                    audioSource.minDistance = 0.1f;
+                    audioSource.maxDistance = 10;
+                    break;
             }
+
+            audioSourceObject.transform.position = collision.GetContact(0).point;
+            audioSource.outputAudioMixerGroup = sfx;
+            audioSource.Play();
 
             // Sets arrow rb to kinematic
             arrowRB.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
@@ -106,7 +161,6 @@ namespace Com.MorganHouston.Imprecision
                 
             }
         }
-
 
     }
 }
