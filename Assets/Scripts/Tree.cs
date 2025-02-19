b5df9d41875d7e22aa54cbed8cec2ff2f4c9060f
Morgan Houston <mmhouston6@live.com>
2025-02-18 20:02:32 -0600

    Fixed Tree Colliders

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -10,15 +10,26 @@ namespace Com.MorganHouston.Imprecision
         public float maxScale = 1.5f;
         public float yPosition = -0.1f;
 
+        [InspectorButton("SetRandomRotation")]
+        public bool setRandomRotation;
+        [InspectorButton("SetRandomScale")]
+        public bool setRandomScale;
 
         // Start is called before the first frame update
         void Awake()
         {
-            transform.position = new Vector3(transform.position.x, yPosition, transform.position.z);
+            //transform.position = new Vector3(transform.position.x, yPosition, transform.position.z);
 
+        }
+
+        public void SetRandomRotation()
+        {
             float randomRot = Random.Range(0.0f, 360.0f);
             transform.rotation = Quaternion.Euler(new Vector3(0, randomRot, 0));
+        }
 
+        public void SetRandomScale()
+        {
             float randomScale = Random.Range(minScale, maxScale);
             transform.localScale = Vector3.one * randomScale;
         }
